69428105519fe403f99f783e79fa64636d2ccb79
Stephen Monaco <stevemonaco@hotmail.com>
2019-08-07 01:08:41 -0400

    Initial memory leak test

---
+++ Monaco.PathTree.UnitTests/PathTreeMemoryLeakTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monaco.PathTree.UnitTests
+{
+    [TestFixture]
+    public class PathTreeMemoryLeakTests
+    {
+        [Test]
+        public void RemoveNode_FreesMemory()
+        {
+            var tree = new PathTree<string>();
+
+            tree.Add("Level1", "TopNode");
+            tree.TryGetNode("Level1", out var topNode);
+
+            var midNode = new PathTreeNode<string>("Level2", "MidNode");
+            var midRef = new WeakReference(midNode);
+
+            var leafNode = new PathTreeNode<string>("Level3", "LeafNode");
+            var leafRef = new WeakReference(leafNode);
+
+            midNode.AttachChild(leafNode);
+            topNode.AttachChild(midNode);
+
+            topNode.RemoveChild("Level2");
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsFalse(midRef.IsAlive);
+                Assert.IsFalse(leafRef.IsAlive);
+            });
+
+        }
+    }
+}
