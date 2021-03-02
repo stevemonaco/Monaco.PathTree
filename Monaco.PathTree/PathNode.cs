623d22523d54db7d9372b4b16d573bdb822bd9ea
Stephen Monaco <stevemonaco@hotmail.com>
2021-03-01 19:59:02 -0500

    Removed single parameter PathNode

--- Monaco.PathTree/PathNode.cs
+++ Monaco.PathTree/PathNode.cs
@@ -2,19 +2,6 @@
 
 namespace Monaco.PathTree
 {
-    public sealed class PathNode<TItem> : PathNodeBase<PathNode<TItem, EmptyMetadata>, TItem, EmptyMetadata>
-    {
-        public PathNode(string rootNodeName, TItem item, EmptyMetadata metadata = default) :
-            base(rootNodeName, item, metadata)
-        {
-        }
-
-        protected override PathNode<TItem, EmptyMetadata> CreateNode(string nodeName, TItem item, EmptyMetadata metadata = default)
-        {
-            return new PathNode<TItem, EmptyMetadata>(nodeName, item, metadata);
-        }
-    }
-
     public sealed class PathNode<TItem, TMetadata> : PathNodeBase<PathNode<TItem, TMetadata>, TItem, TMetadata>
     {
         public PathNode(string rootNodeName, TItem item, TMetadata metadata = default) :
