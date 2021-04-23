e6e5c4e8c01e900a5314ec188bb643cd3f64c71b
Stephen Monaco <stevemonaco@hotmail.com>
2021-04-23 11:01:11 -0400

    Add build system

---
+++ build/Build.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[ShutdownDotNetAfterServerBuild]
+[GitHubActions("ci",
+    GitHubActionsImage.WindowsLatest,
+    PublishArtifacts = true,
+    On = new[] { GitHubActionsTrigger.Push },
+    InvokedTargets = new[] { nameof(Test) }
+)]
+class Build : NukeBuild
+{
+    public static int Main() => Execute<Build>(x => x.Publish);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+
+    Project PathTreeProject => Solution.GetProject("Monaco.PathTree");
+    Project TestProject => Solution.GetProject("Monaco.PathTree.UnitTests");
+
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+
+    Target Clean => _ => _
+        .Executes(() =>
+        {
+            var dirs = RootDirectory.GlobDirectories("**/bin", "**/obj")
+                .Where(x => !x.ToString().Contains("\\build\\"));
+
+            dirs.ForEach(DeleteDirectory);
+            EnsureCleanDirectory(OutputDirectory);
+        });
+
+    Target Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(_ => _
+                .SetProjectFile(TestProject)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore()
+                .EnableNoBuild());
+        });
+
+    Target Package => _ => _
+        .DependsOn(Test)
+        .Executes(() =>
+        {
+            DotNetPack(_ => _
+                .SetProject(PathTreeProject)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore()
+                .EnableNoBuild()
+                .SetOutputDirectory(OutputDirectory));
+        });
+
+    Target Publish => _ => _
+        .DependsOn(Package)
+        .Executes(() =>
+        {
+            DotNetPublish(_ => _
+                .SetProject(PathTreeProject)
+                .EnableNoRestore()
+                .EnableNoBuild()
+                .SetConfiguration(Configuration)
+                .SetOutput(OutputDirectory));
+        });
+}
